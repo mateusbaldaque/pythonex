3717d18c68b67e1386359be3d19618eddee31735
Mateus <mateus.baldaque.2001@gmail.com>
2023-10-08 15:12:51 +0100

    adiciono ex 1

--- QP03/exercise1.py
+++ QP03/exercise1.py
@@ -1,9 +1,18 @@
 n = int(input())
 i = 1
-while i < n:
-    res = i * n
-    print(n, "x", i, '=', res )
-    i += 1
+a = 1
+if n == 0:
+    while a <= 10:
+        res3 = a * n
+        print(n, "x", a, '=', res3 )
+        a += 1
+else:
+    while i < n and i <= 10:
+        res = i * n
+        print(n, "x", i, '=', res )
+        i += 1
+    
 
-res2 = n ** 2
-print(n, '^ 2 =', res2) 
+if 0 < n <= 10:
+    res2 = n ** 2
+    print(n, '^ 2 =', res2) 
